d6a035f9964bfd1981d70956fb776dd2e875fc38
EskimoBrew33 <f.taillefer@icloud.com>
2024-12-09 22:49:40 -0500

    renamed projects

--- web-app/src/App.jsx
+++ web-app/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import DynamicForm from './components/ui/DynamicForm';
+import DynamicForm from './components/ui/dynamicForm';
 
 function App() {
   const [productConfig, setProductConfig] = useState([]);
